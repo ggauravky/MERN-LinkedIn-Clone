5e3f694be2918d0baeb79cc1bca970bdc945ede3
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-31 02:15:59 +0530

    added image upload and setup route for update profile

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,29 +1,80 @@
-import User from '../models/user.model.js';
+import User from "../models/user.model.js";
+import cloudinary from "../utils/cloudinary.js";
 
 export const getSuggestConnections = async (req, res) => {
-    try{
-        const currentUser=await User.findById(req.user._id).select('connections');
-        const suggestedUser=await User.find({
-            _id:{$ne:req.user._id},
-            _id:{$nin:currentUser.connections}
-        }).select('name username profilePicture headline').limit(4);
-        res.status(200).json(suggestedUser);
-    }catch(error){
-        console.error("Error fetching suggested connections:", error);
-        res.status(500).json({ message: "Error fetching suggested connections" });
-    }
-}
+  try {
+    const currentUser = await User.findById(req.user._id).select("connections");
+    const suggestedUser = await User.find({
+      _id: { $ne: req.user._id },
+      _id: { $nin: currentUser.connections },
+    })
+      .select("name username profilePicture headline")
+      .limit(4);
+    res.status(200).json(suggestedUser);
+  } catch (error) {
+    console.error("Error fetching suggested connections:", error);
+    res.status(500).json({ message: "Error fetching suggested connections" });
+  }
+};
 
 export const getPublicProfile = async (req, res) => {
-    try {
-        const user = await User.findOne({ username: req.params.username }).select('-password');
-        if (!user) {
-            return res.status(404).json({ message: "User not found" });
-        }
-        res.status(200).json(user);
-    } catch (error) {
-        console.error("Error fetching public profile:", error);
-        res.status(500).json({ message: "Error fetching public profile" });
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      "-password",
+    );
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json(user);
+  } catch (error) {
+    console.error("Error fetching public profile:", error);
+    res.status(500).json({ message: "Error fetching public profile" });
+  }
+};
+
+export const updateProfile = async (req, res) => {
+  try {
+    const allowedFields = [
+      "name",
+      "profilePicture",
+      "bannerImg",
+      "headline",
+      "location",
+      "about",
+      "skills",
+      "experience",
+      "education",
+      "username",
+    ];
+
+    const updatedData = {};
+
+    for (const field of allowedFields) {
+      if (req.body[field]) {
+        updatedData[field] = req.body[field];
+      }
     }
 
+    if(req.body.profilePicture) {
+        const result = await cloudinary.uploader.upload(req.body.profilePicture);
+        updatedData.profilePicture = result.secure_url;
     }
+
+    if(req.body.bannerImg) {
+        const result = await cloudinary.uploader.upload(req.body.bannerImg);
+        updatedData.bannerImg = result.secure_url;
+    }
+
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      { $set: updatedData },
+      { new: true },
+    ).select("-password");
+
+    res.json(user);
+
+  } catch (error) {
+    console.error("Error updating profile:", error);
+    res.status(500).json({ message: "Error updating profile" });
+  }
+};
