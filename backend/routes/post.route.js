c30b544a1456995650f66d614e1f446609b936b5
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-08-02 23:58:17 +0530

    Update post.route.js

--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { getFeedPosts, createPost ,deletePost ,getPostById ,createComment } from '../controllers/post.controller.js';
+import { getFeedPosts, createPost ,deletePost ,getPostById ,createComment ,likePost } from '../controllers/post.controller.js';
 
 const router=express.Router();
 
