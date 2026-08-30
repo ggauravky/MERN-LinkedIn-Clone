a1527b837c46ad1ef529307ecff46c8327c326eb
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-08-31 00:31:32 +0530

    worked on notification page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,8 @@ import LoginPage from "./pages/auth/LoginPage";
 import toast, { Toaster } from "react-hot-toast";
 import { useQuery } from "@tanstack/react-query";
 import { axiosInstance } from "./lib/axios";
+import NotificationsPage from "./pages/NotificationsPage";
+
 
 function App() {
   const { data: authUser, isPending } = useQuery({
@@ -45,6 +47,10 @@ function App() {
         <Route
           path="/login"
           element={!authUser ? <LoginPage /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/notifications"
+          element={authUser ? <NotificationsPage /> : <Navigate to="/login" />}
         />
       </Routes>
       <Toaster />
