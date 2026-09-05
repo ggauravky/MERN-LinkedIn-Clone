e6c5b7c3ecdaf15fbe43bc5478e040a8ae00baf8
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-09-06 01:31:40 +0530

    fix the vite error

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ app.use("/api/v1/notifications", notificationRoutes);
 app.use("/api/v1/connections", connectionRoutes);
 
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "frontend", "dist")));
 
-	app.get("*", (req, res) => {
+	app.get("{*splat}", (req, res) => {
 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 	});
 }
